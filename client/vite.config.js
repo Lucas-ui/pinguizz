027b63f8ab92025d27e5d871c2269555df0abd19
Lucas-ui <lucas7500@hotmail.fr>
2025-05-20 12:00:11 +0200

    Fix: port docker vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,4 +13,18 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: true,
+    port: 5173,
+  },
+  hmr: {
+    protocol: "ws",
+    host: "host.docker.internal",
+    port: 5173,
+  },
+  watch: {
+    usePolling: true,
+    interval: 1000,
+    binaryInterval: 3000,
+  },
 });
