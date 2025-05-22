3a32fd443c4e67ea2d5f2558b736ba1c1fe7a5f7
Lucas-ui <lucas7500@hotmail.fr>
2025-05-22 15:51:15 +0200

    Feat: route users api

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -4,6 +4,22 @@ export const deleteUser = () => {
   return apiClient.delete("users");
 };
 
-export const editPassword = () => {
-  return apiClient.post("users/password");
+export const editPassword = (data) => {
+  return apiClient.put("users/password", data);
+};
+
+export const editName = (data) => {
+  return apiClient.put("users/name", data);
+};
+
+export const editFirstname = (data) => {
+  return apiClient.put("users/firstname", data);
+};
+
+export const editUsername = (data) => {
+  return apiClient.put("users/username", data);
+};
+
+export const allUsers = () => {
+  return apiClient.get("users/all");
 };
